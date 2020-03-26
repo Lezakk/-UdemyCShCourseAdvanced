6e12a00904cf09e4eddb5c8a952b8c4d40950580
Lezakk <wiktro.zak94@gmail.com>
2020-03-26 22:40:15 +0100

    Initial commit

---
+++ UdemyClassesAdvanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdemyClassesAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
